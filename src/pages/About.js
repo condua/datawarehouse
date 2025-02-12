7482e68f6ba7c74a308bdc42c3ab16e98f948308
condua <phuc.phanphuc2002@hcmut.edu.vn>
2025-02-12 21:30:29 +0700

    new update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import '../scss/pages/About.scss'
-import logo from '../images/about.jpg'
-import phuc from '../images/phuc.jpg'
-import huy from '../images/anhHuy.jpg'
-import nhan from '../images/nhan.jpg'
-import phucanh from '../images/phucanh.jpg'
+import React from "react";
+import "../scss/pages/About.scss";
+import logo from "../images/about.jpg";
+import phuc from "../images/phuc.jpg";
+import huy from "../images/anhHuy.jpg";
+import nhan from "../images/nhan.jpg";
+import phucanh from "../images/phucanh.jpg";
 const About = () => {
-    return (
-        <div className="About">
-            <div className='container-about'>
-                <div className='left-side'>
-                    <h1>Our mission</h1>
-                    <p>Tại <span style={{color:'blue',fontWeight:'bolder'}}>Mybk Technology</span>, sứ mệnh của chúng tôi là tiếp bước cho học sinh trên hành trình tri thức. Bằng việc cung cấp cho học sinh các công cụ và thông tin cần thiết, chúng tôi giúp học sinh định hướng tìm được ngôi trường đại học phù hợp với nguyện vọng và năng lực học tập của mình.</p>
-                </div>
-                <img className='right-side' alt='' src='https://collegecost.ed.gov/wwwroot/fonts/graphic-scorecard.svg' />
-                
-            </div>
+  return (
+    <div className="About">
+      <div className="container-about">
+        <div className="left-side">
+          <h1>Our mission</h1>
+          <p>
+            Tại{" "}
+            <span style={{ color: "blue", fontWeight: "bolder" }}>
+              Mybk Technology
+            </span>
+            , sứ mệnh của chúng tôi là tiếp bước cho học sinh trên hành trình
+            tri thức. Bằng việc cung cấp cho học sinh các công cụ và thông tin
+            cần thiết, chúng tôi giúp học sinh định hướng tìm được ngôi trường
+            đại học phù hợp với nguyện vọng và năng lực học tập của mình.
+          </p>
+        </div>
+        <img
+          className="right-side"
+          alt=""
+          src="https://collegecost.ed.gov/wwwroot/fonts/graphic-scorecard.svg"
+        />
+      </div>
 
-            <div style={{width:'80%',margin:'auto',borderBottom:'1px dashed black'}}></div>
-            <p style={{textAlign:'center',marginBottom:'-20px'}}>💎</p>
-            <h1 style={{textAlign:'center'}}>Team members</h1>
-            {/* <div className='team-members'>
+      <div
+        style={{
+          width: "80%",
+          margin: "auto",
+          borderBottom: "1px dashed black",
+        }}
+      ></div>
+      <p style={{ textAlign: "center", marginBottom: "-20px" }}>💎</p>
+      {/* <h1 style={{textAlign:'center'}}>Team members</h1> */}
+      {/* <div className='team-members'>
                 <div className='members-infor'>
                     <img alt='' className='avatar' src={phuc}/>
                     <h2>Phan Hoàng Phúc</h2>
@@ -41,8 +59,8 @@ const About = () => {
                 </div>
                 
             </div> */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default About;
